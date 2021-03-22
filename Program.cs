1733d41b9940b2d5a465ff69bf6236255350b58c
Maycon-15 <78987309+Maycon-15@users.noreply.github.com>
2021-03-21 21:44:14 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace medidas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string MedidaUsuario;
+            double metros, centimetros, quilometros;
+
+            Console.WriteLine("---------");
+            Console.Write("Coloque abaixo a medida em metro para que ela possa ser transferida para centimetros (cm) e kilometros (KM): ");
+            MedidaUsuario = Console.ReadLine();
+            metros = Convert.ToDouble(MedidaUsuario);
+            centimetros = metros * 100;
+            quilometros = metros / 100;
+
+            
+            Console.WriteLine("----------");
+            Console.ForegroundColor = ConsoleColor.DarkBlue;
+            Console.WriteLine("\n---Equivalencia---");
+            Console . ResetColor ();
+            Console.WriteLine($"{centimetros:N2} cm");
+            Console.WriteLine($"{metros:N1} m");
+            Console.WriteLine($"{quilometros:N3} km");
+            Console.WriteLine("----------");
+        }
+    }
+}
